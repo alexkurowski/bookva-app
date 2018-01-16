e835cef8723622b5880eed2b811a1502d58c7a3f
Alex <alex.zneue@gmail.com>
2018-01-16 19:49:31 +0300

    Add alert modal

---
+++ src/renderer/components/writer/modals/Alert.vue
@@ -0,0 +1,35 @@
+<template lang='slm'>
+  .modal
+    .modal-header {{ modalHeader }}
+    .modal-body {{ modalBody }}
+    .modal-footer
+      .empty
+      .modal-choice.secondary @click='hide'
+</template>
+
+<script>
+  export default {
+    name: 'AlertModal',
+
+    computed: {
+      modalHeader () {
+        return this.$t(`modal.${ Modal.modalContentKey }.header`)
+      },
+
+      modalBody () {
+        return this.$t(`modal.${ Modal.modalContentKey }.body`)
+      },
+    },
+
+    methods: {
+      hide () {
+        this.$store.dispatch('modalHide')
+      }
+    }
+  }
+</script>
+
+<style lang='sass' scoped>
+  .modal
+    max-width: 60%
+</style>
